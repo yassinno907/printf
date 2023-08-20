3abd0fc6167ddcc001b1475d2e7de86a894eda16
yassinno907 <yassine.nopics95@gmail.com>
2023-08-20 11:24:25 -0700

    readme

--- _printf.c
+++ _printf.c
@@ -1,80 +1,66 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdarg.h>
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format.
- * Return: Counter.
- * */
-/**
- * _putchar - print character
- * @c: char variable
- * Return: write(1, &c, 1)
- * */
-/**
- * handle_string - function handling string
- * @*str: a poinnter of type char
- * Return: i
- * */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-int handle_string(char *str)
-{
-int i = 0;
-if (str == NULL)
-{
-handle_string("(null)");
-return;
-}
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-return (i);
-}
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-int counter = 0;
-va_list args;
-va_start(args, format);
-if (!format || !format[0])
-return (-1);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-char c = va_arg(args, int);
-counter += _putchar(c);
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
-else if (*format == 's')
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
 {
-char *str = va_arg(args, char *);
-counter += handle_string(str);
-}
-else if (*format == '%')
-{
-_putchar('%');
-counter++;
-if (*format)
-{
-_putchar(*format);
-counter++;
-}
-}
-format++;
-}
-else
-{
-_putchar(*format);
-format++;
-counter++;
-}
-}
-return (counter);
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
