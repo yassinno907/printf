6c42516d496cf79d127f5cd2419ed32fcd69ab92
ilias <iliaschokairi@gmail.com>
2023-08-22 18:46:25 +0100

    test printf

--- myprintf.c
+++ myprintf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 
 	if (!format  || !format[0])
 		return (-1);
-	if (format[0] && format[1] == ' ' &&
+	if (format[0] == '%' && format[1] == ' ' &&
 	format[2] == '\0')
 		return (-1);
 	va_start(my_char, format);
