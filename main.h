7ecdc55ac062c45e1771847358be9148ecc1e90f
yassinno907 <yassine.nopics95@gmail.com>
2023-08-24 09:10:25 -0700

    readme

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
-int my_printf(const char *format, ...)
+#define MAIN
 int _printf(const char *format, ...);
 int  print_number(long int n);
 int print_string(char *s);
